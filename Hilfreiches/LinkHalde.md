7532128c5a349e567b03c8c10cbfb9ce71136c8a
Stefan Ober <StefanO@ppedv.de>
2023-09-20 17:03:13 +0200

    Abschluss

---
+++ Hilfreiches/LinkHalde.md
@@ -0,0 +1,13 @@
+# Hilfreiche Links
+
+[Hilfe zu Parameter](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-5.1#attributes-of-parameters)
+
+[Comment Based Help](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-5.1#comment-based-help-keywords)
+
+[Standardisierte Verben](https://learn.microsoft.com/en-us/powershell/scripting/developer/cmdlet/approved-verbs-for-windows-powershell-commands?view=powershell-5.1)
+
+[PSProfile](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-5.1)
+
+[Prompt](https://learn.microsoft.com/de-de/powershell/module/microsoft.powershell.core/about/about_prompts?view=powershell-5.1)
+
+[Class](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_classes?view=powershell-5.1)
---
+++ Skriptbeispiel/Get-SecEvents.ps1
@@ -0,0 +1,51 @@
+﻿<#
+.SYNOPSIS
+ Kurzbeschreibung
+.DESCRIPTIOn
+ LAnge Bechreibung
+.PARAMETER EventId
+ 4624 Anmeldung
+ 4625 fehlgeschlagene Anmeldung
+ 4634 Abmeldung
+.EXAMPLE
+.\Get-SecEvents.ps1 -EventId 4634 -Newest 7
+
+   Index Time          EntryType   Source                 InstanceID Message
+   ----- ----          ---------   ------                 ---------- -------
+  589563 Sep 20 16:02  SuccessA... Microsoft-Windows...         4634 Ein Konto wurde abgemeldet....
+  589562 Sep 20 16:02  SuccessA... Microsoft-Windows...         4634 Ein Konto wurde abgemeldet....
+  589561 Sep 20 16:02  SuccessA... Microsoft-Windows...         4634 Ein Konto wurde abgemeldet....
+  589558 Sep 20 16:02  SuccessA... Microsoft-Windows...         4634 Ein Konto wurde abgemeldet....
+  589555 Sep 20 16:02  SuccessA... Microsoft-Windows...         4634 Ein Konto wurde abgemeldet....
+  589554 Sep 20 16:02  SuccessA... Microsoft-Windows...         4634 Ein Konto wurde abgemeldet....
+  589543 Sep 20 16:01  SuccessA... Microsoft-Windows...         4634 Ein Konto wurde abgemeldet....
+.LINK
+https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-5.1
+#>
+[cmdletBinding(PositionalBinding=$false)]
+param(
+
+[Parameter(Mandatory=$true)]
+[ValidateSet(4624,4625,4634)]
+[int]$EventId,
+
+[ValidateRange(5,10)]
+[int]$Newest = 5,
+
+[ValidateScript({Test-NetConnection -Computername $PSITem -CommonTCPPort WinRm})]
+[string]$Computername = "localhost"
+)
+
+Write-Verbose -Message "Vom User wurden folgende Werte übergeben $Eventid , $Newest , $Computername"
+Write-Debug -Message "Vor Abfrage"
+$geteventlogerror = ""
+try
+{
+    Get-EventLog -LogName Security -ComputerName $Computername -ErrorVariable geteventlogerror -ErrorAction Stop | Where-Object EventId -eq $EventId | Select-Object -First $Newest 
+}
+catch
+{
+    Out-File -InputObject $geteventlogerror -FilePath C:\Testfiles\geteventlogerror.log -Append
+}
+
+Out-File -InputObject $Error -FilePath C:\Testfiles\allerror.log -Append
